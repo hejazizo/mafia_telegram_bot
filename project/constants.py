335d80284ce229e567b75cd743c3ceffa0431798
hejazizo <hejazizo@ualberta.ca>
2021-01-04 16:33:44 -0700

    fixed bug in relative path

--- project/constants.py
+++ project/constants.py
@@ -1,4 +1,6 @@
 import json
+from data import DATA_PATH
+from pathlib import Path
 
 BOT_ID = 73106435
-ROLES = json.load(open("./data/roles.json"))
+ROLES = json.load(open(Path(DATA_PATH, "roles.json")))
---
+++ project/data/__init__.py
@@ -0,0 +1,2 @@
+from pathlib import Path
+DATA_PATH = Path(__file__).parent
